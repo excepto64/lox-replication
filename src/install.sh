3b4622a6c6095207d0398ae859ffc8a31be49ff8
Adam_H <adam.harrison137@outlook.com>
2026-06-11 10:21:43 +0100

    Fix dependecies for CUDA 12.8.

--- src/install.sh
+++ src/install.sh
@@ -18,8 +18,8 @@ source .venv/bin/activate
 export UV_LINK_MODE=copy
 
 # Install packages
-uv pip install "torch==2.12.0" --index-url https://download.pytorch.org/whl/cu126
-uv pip install "https://github.com/mjun0812/flash-attention-prebuild-wheels/releases/download/v0.9.17/flash_attn-2.8.3+cu126torch2.12-cp312-cp312-linux_x86_64.whl"
+uv pip install "torch==2.11.0" --index-url https://download.pytorch.org/whl/cu128
+uv pip install "https://github.com/mjun0812/flash-attention-prebuild-wheels/releases/download/v0.9.4/flash_attn-2.8.3+cu128torch2.11-cp312-cp312-linux_x86_64.whl"
 uv pip install openrlhf --no-build-isolation
 uv pip install nvidia-ml-py
 uv pip uninstall pynvml
