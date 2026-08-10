fd529092d5c2777499275d5209e00f5fc762cf2e
Adam_H <adam.harrison137@outlook.com>
2026-08-10 16:18:35 +0100

    Add sgd-dpo safety measurement.

--- src/run_stage_A.sh
+++ src/run_stage_A.sh
@@ -14,9 +14,9 @@ echo $(date)
 # echo "Model fine-tune complete."
 
 # Measure the safety update and calculate gini of the update.
-echo "Initiate update measurement."
-./src/measure_update.sh ${SCRATCH} ${seed} ${cluster} ${config} 1
-echo "Update measurement complete."
+# echo "Initiate update measurement."
+# ./src/measure_update.sh ${SCRATCH} ${seed} ${cluster} ${config} 1
+# echo "Update measurement complete."
 
 # Measure the safety of the model pre-attack
 echo "Initiate safety measurement."
