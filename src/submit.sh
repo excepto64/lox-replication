285b0fef9aeadd8d6af61058b9c8742a0ecdf6ec
Adam_H <adam.harrison137@outlook.com>
2026-08-11 18:18:44 +0100

    Submit runs for attacks.

--- src/submit.sh
+++ src/submit.sh
@@ -5,14 +5,16 @@
 
 SCRATCH=/disk/scratch/s2028118
 
-runs=("configs/lox_Llama-3_2-1B_r0_1e_dpo_adam.cfg" "configs/lox_Llama-3_2-1B_r0_1e_dpo_sgd.cfg" \
-    "configs/lox_Llama-3_2-1B_r0_1e_sft_adam.cfg" "configs/lox_Llama-3_2-1B_r0_1e_sft_sgd.cfg" \
-    "configs/lox_Llama-3_2-3B_r0_1e_dpo_adam.cfg" "configs/lox_Llama-3_2-3B_r0_1e_dpo_sgd.cfg" \
-    "configs/lox_Llama-3_2-3B_r0_1e_sft_adam.cfg" "configs/lox_Llama-3_2-3B_r0_1e_sft_sgd.cfg" )
+runs=( \
+    "configs/lox_Llama-3_2-1B_r0_1e_dpo_adam.cfg" \
+    "configs/lox_Llama-3_2-1B_r0_1e_dpo_sgd.cfg" \
+    "configs/lox_Llama-3_2-1B_r0_1e_sft_adam.cfg" \
+    "configs/lox_Llama-3_2-1B_r0_1e_sft_sgd.cfg" \
+)
 
 seeds=(2 0 26)
 cluster=1
-stage="A"
+stage="B"
 
 echo Script started.
 
